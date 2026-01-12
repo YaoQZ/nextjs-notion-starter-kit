55e1d75e8cf3246266fe05ee549c2f4cc129d0d1
YaoQZ <80189304+YaoQZ@users.noreply.github.com>
2026-01-12 22:44:13 +0900

    Update [pageId].tsx

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -5,6 +5,7 @@ import { domain, isDev } from '@/lib/config'
 import { getSiteMap } from '@/lib/get-site-map'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
 import { type PageProps, type Params } from '@/lib/types'
+export const runtime = 'edge'
 
 export const getStaticProps: GetStaticProps<PageProps, Params> = async (
   context
